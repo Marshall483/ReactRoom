1cecdaf350e010f9d41e91c0c9c3363f7d9be2f6
Alexander <jows.17@mail.ru>
2021-07-12 15:44:17 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { AppBar, Container, Toolbar, IconButton, Typography, Box } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
-import ClassComp from './ClassComp.js';
+// import ClassComp from './ClassComp.js';
 
+const useStyles = makeStyles((theme) => ({
+  root:{
+    flexGrow: 1
+  },
+  menuButton:{
+    marginRight: theme.spacing(1) // 1 == 8px
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppBar position='fixed'>
+      <Container fixed>
 
-        <ClassComp />
+        <Toolbar >
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <IconButton edge='start' className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant='h7' className={classes.title}>Мастер бронирования переговорных</Typography>
+
+          <Box mr={3}>
+            <Button color='black' variant='outlined'>Войти</Button>
+          </Box>
+
+          <Button color='secondary' variant='contained'>Регистрация</Button>
+
+        </Toolbar>
+        
+      </Container>
+    </AppBar>
   );
 }
 
--- src/ClassComp.js
+++ src/ClassComp.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import { 
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-import Home from './Home.js'
-import Users from './Users.js';
-import About from './About.js';
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import Home from './Home.js'
+// import Users from './Users.js';
+// import About from './About.js';
 
 // import First from './Fscomponent.js'
 // import Second from './SecondFsComp.js'
@@ -29,24 +24,7 @@ export default class ClassComp extends Component {
     render()
     {
         return(
-            <Router>
-                <div>
-                    <nav>
-                        <ul>
-                            <li> <Link to="/">Home</Link></li>
-                            <li> <Link to="/about">About</Link></li>
-                            <li> <Link to="/users">Users</Link></li>
-                        </ul>
-                    </nav>
-
-                    <Switch>
-                        <Route path="/About"> <About /> </Route>
-                        <Route path="/Users"> <Users /> </Route>
-                        <Route path="/"> <Home /> </Route>
-                    </Switch>
-
-                </div>
-            </Router>
+          <h1>Nothing here.</h1>  
         );
     }
 }
