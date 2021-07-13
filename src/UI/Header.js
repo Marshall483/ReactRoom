ab7dd44e5f23bed58c4c792698831d115200e5fc
Alexander <jows.17@mail.ru>
2021-07-13 13:11:17 +0300

    Header

---
+++ src/UI/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { AppBar, Container, Toolbar, IconButton, Typography, Box } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+const setStyles = makeStyles((theme) => ({
+    menuButton:{
+      marginRight: theme.spacing(1) // 1 == 8px
+    },
+    title: {
+      flexGrow: 1
+    }
+  }))
+
+export default function Header() {
+    const headerStyles = setStyles()
+    return (
+        <AppBar position='fixed' style={{ backgroundColor: 'gray'}} >
+                <Container fixed>
+
+                    <Toolbar >
+
+                    <IconButton edge='start' className={headerStyles.menuButton}>
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Typography variant='h7' className={headerStyles.title}>Мастер бронирования переговорных</Typography>
+
+                    <Box mr={3}>
+                        <Button color='black' variant='outlined'>Войти</Button>
+                    </Box>
+
+                    <Button color='secondary' variant='contained'>Регистрация</Button>
+
+                    </Toolbar>
+                    
+                </Container>
+            </AppBar>
+    )
+}
